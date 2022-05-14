c7a63ab2f80c5b3d922431c0f45c18b62f5d9bb5
Amalendu Pandey <amalendupandey56@gmail.com>
2022-05-14 15:32:02 +0530

    assignment complete

--- src/App.js
+++ src/App.js
@@ -1,52 +1,81 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   //eslint-disable-next-line
   const [randomBuyList, setRandomBuyList] = useState([]);
   const [randomSellList, setRandomSellList] = useState([]);
-  const [clickedBuyList, setClickedBuyList] = useState([])
-  const generateRandomQuantity = ( min=35000, max=36000) => {
+  const [clickedBuyList, setClickedBuyList] = useState([]);
+  const generateRandomQuantity = (min = 35000, max = 36000) => {
     let difference = max - min;
     let rand = Math.random();
     rand = Math.floor(rand * difference);
     rand = rand + min;
     return rand;
-  }
+  };
   const getRandomBuyList = () => {
-    const randomBuyList = [];
     let buyRate = Math.floor(Math.random() * 10);
     let buyQuantity = generateRandomQuantity();
-    randomBuyList.push(buyRate, buyQuantity);
-    setRandomBuyList(randomBuyList);
-    
-  }
+    setRandomBuyList([...randomBuyList, { qty: buyQuantity, rate: buyRate }]);
+    console.log(randomBuyList);
+  };
 
   const getRandomSellList = () => {
-    const randomSellList = [];
+    const sellList = [];
     let sellRate = Math.floor(Math.random() * 10);
     let sellQuantity = generateRandomQuantity();
-    randomSellList.push(sellRate, sellQuantity);
-    setRandomSellList(randomBuyList);
-  }
+    sellList.push(sellRate, sellQuantity);
+    setRandomSellList([
+      ...randomSellList,
+      { qty: sellQuantity, rate: sellRate },
+    ]);
+    console.log(randomSellList);
+  };
 
   return (
     <div>
-      <h3 style={{ fontWeight: "bold" }}>Buy Sell</h3>
-      <div className="buttons">
-        <div className="buy items">
-          <button type="button" onClick={() => getRandomBuyList()}>
-            Buy
-          </button>
-        </div>
-        <div className="sell items">
-          <button type="button" onClick={() => getRandomSellList()}>
-            Sell
-          </button>
+      <div className="mb-[0.09rem]">
+        <h3 className="mb-5"  style={{ fontWeight: "bold" }}>Buy Sell</h3>
+        <div className="buttons">
+          <div className="buy items">
+            <button type="button" onClick={() => getRandomBuyList()}>
+              Buy
+            </button>
+          </div>
+          <div className="sell items">
+            <button type="button" onClick={() => getRandomSellList()}>
+              Sell
+            </button>
+          </div>
         </div>
       </div>
-     
+      <div className="sell items flex flex-col">
+        <div className="buy-list w-full">
+          {randomBuyList?.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-row-reverse justify-between bg-black text-green-500 font-bold text-[1.5rem] box"
+            >
+              <p className="mr-28">{item.qty}</p>
+              <p className="ml-28">{item.rate}</p>
+            </div>
+          ))}
+        </div>
+        <hr className="bg-black w-1 m-1 hline" />
+
+        <div className="sell-list w-full">
+          {randomSellList?.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-row-reverse justify-between bg-black text-red-500 font-bold text-[1.5rem] box"
+            >
+              <p className="mr-28">{item.qty}</p>
+              <p className="ml-28">{item.rate}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
